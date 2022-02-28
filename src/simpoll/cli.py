507627c987932dfd7730ad59db93c7360e46201d
rhdzmota <rhdzmota@gmail.com>
2022-02-27 19:34:42 -0600

    Run frontend command

--- src/simpoll/cli.py
+++ src/simpoll/cli.py
@@ -1,7 +1,11 @@
+import os
+import sys
 import time
 import json
+import textwrap
 from typing import Any, Optional
 
+from .utils import temp_file
 from .settings import (
     get_logger,
     SIMPOLL_DEFAULT_HELLO_WORLD,
@@ -23,3 +27,30 @@ class CLI:
     def hello(self, world: Optional[str] = None):
         world = world or SIMPOLL_DEFAULT_HELLO_WORLD
         self.display(f"Hello, {world}!")
+
+    def frontend(self):
+        import importlib
+
+        # Create temporal path
+        temp_file_path = temp_file(
+            suffix=".py",
+            content=textwrap.dedent(
+                """
+                from simpoll.frontend.main import main
+
+
+                if __name__ == "__main__":
+                    main()
+                """
+            )
+        )
+        try:
+            streamlit_cli = importlib.import_module("streamlit.cli")
+            sys.argv = ["streamlit", "run", temp_file_path]
+            streamlit_cli.main()
+        except Exception as e:
+            logger.error(e)
+        finally:
+            if os.path.isfile(temp_file_path):
+                os.remove(temp_file_path)
+                logger.warning("Temporary file correctly deleted: %s", temp_file_path)
